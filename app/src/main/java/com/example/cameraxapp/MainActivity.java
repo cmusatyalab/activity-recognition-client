c68320592d362ed9a5356fabaedfbb0b1579ae0e
Roger Iyengar <ri@rogeriyengar.com>
2019-11-20 16:19:31 -0500

    Got Video Upload working

--- app/src/main/java/com/example/cameraxapp/MainActivity.java
+++ app/src/main/java/com/example/cameraxapp/MainActivity.java
@@ -3,53 +3,56 @@ package com.example.cameraxapp;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraX;
-import androidx.camera.core.ImageAnalysis;
-import androidx.camera.core.ImageAnalysisConfig;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureConfig;
-import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.core.PreviewConfig;
 import androidx.camera.core.VideoCapture;
 import androidx.camera.core.VideoCaptureConfig;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LifecycleOwner;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.util.Log;
-import android.util.Rational;
 import android.util.Size;
 import android.graphics.Matrix;
 
 import java.io.File;
-import java.nio.ByteBuffer;
 import java.nio.file.Files;
-import java.time.Clock;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import android.os.Bundle;
-import android.view.Surface;
 import android.view.TextureView;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import com.google.protobuf.ByteString;
+
+import edu.cmu.cs.gabriel.client.comm.ServerComm;
+import edu.cmu.cs.gabriel.client.comm.ServerCommCore;
+import edu.cmu.cs.gabriel.client.function.Consumer;
+
+import edu.cmu.cs.gabriel.protocol.Protos.ResultWrapper;
+import edu.cmu.cs.gabriel.protocol.Protos.FromClient;
+import edu.cmu.cs.gabriel.protocol.Protos.PayloadType;
 
 @SuppressLint("RestrictedApi")
 public class MainActivity extends AppCompatActivity {
 
     private final int REQUEST_CODE_PERMISSIONS = 10;
     private static final String TAG = "CameraXApp";
+    private static final String ENGINE_NAME = "activity_recognition";
+    // private static final String SERVER_IP = "deluge.elijah.cs.cmu.edu";
+    private static final String SERVER_IP = "gs17934.sp.cs.cmu.edu";
 
     private String[] REQUIRED_PERMISSIONS = new String[] {
             Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO
     };
+
+    private TextureView viewFinder;
+    private Executor executor = Executors.newSingleThreadExecutor();
+    private ServerCommCore serverCommCore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,30 +68,35 @@ public class MainActivity extends AppCompatActivity {
                     this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
         }
 
-        // Every time the provided texture view changes, recompute layout
-        viewFinder.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+        Consumer<ResultWrapper> consumer = new Consumer<ResultWrapper>() {
             @Override
-            public void onLayoutChange(
-                    View v, int left, int top, int right, int bottom, int oldLeft, int oldTop,
-                    int oldRight, int oldBottom) {
-                updateTransform();
-            }
-        });
-    }
+            public void accept(ResultWrapper resultWrapper) {
 
-    private TextureView viewFinder;
-    private Executor executor = Executors.newSingleThreadExecutor();
+            }
+        };
+
+        Runnable onDisconnect = new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
+
+        serverCommCore = new ServerComm(consumer, onDisconnect, SERVER_IP,
+                9099, getApplication());
+    }
 
     private Runnable startCamera = new Runnable() {
         @Override
         public void run() {
             // Create configuration object for the viewfinder use case
-            PreviewConfig previewConfig = new PreviewConfig.Builder()
-                    .setTargetResolution(new Size(640, 480))
-                    .build();
+            PreviewConfig previewConfig = new PreviewConfig.Builder().setTargetResolution(
+                    new Size(640, 480)).build();
 
             // Build the viewfinder use case
             Preview preview = new Preview(previewConfig);
+
+            Matrix matrix = new Matrix();
 
             // Every time the viewfinder is updated, recompute layout
             preview.setOnPreviewOutputUpdateListener(
@@ -99,10 +107,10 @@ public class MainActivity extends AppCompatActivity {
                     parent.addView(viewFinder, 0);
 
                     viewFinder.setSurfaceTexture(previewOutput.getSurfaceTexture());
-                    updateTransform();
                 });
 
-            VideoCaptureConfig videoCaptureConfig = new VideoCaptureConfig.Builder().build();
+            VideoCaptureConfig videoCaptureConfig = new VideoCaptureConfig.Builder()
+                    .setTargetResolution(new Size(640, 480)).build();
             VideoCapture videoCapture = new VideoCapture(videoCaptureConfig);
 
             findViewById(R.id.capture_button).setOnClickListener(view -> {
@@ -112,6 +120,13 @@ public class MainActivity extends AppCompatActivity {
                     public void onVideoSaved(File file) {
                         try {
                             byte[] fileContent = Files.readAllBytes(file.toPath());
+
+                            FromClient.Builder fromClientBuilder = FromClient.newBuilder();
+                            fromClientBuilder.setPayloadType(PayloadType.VIDEO);
+                            fromClientBuilder.setEngineName(ENGINE_NAME);
+                            fromClientBuilder.setPayload(ByteString.copyFrom(fileContent));
+
+                            serverCommCore.sendBlocking(fromClientBuilder);
                         } catch (java.io.IOException e) {
                             Log.e(TAG, "Exception reading video file", e);
                         }
@@ -125,44 +140,13 @@ public class MainActivity extends AppCompatActivity {
                         Log.e(TAG, msg, cause);
                     }
                 });
-
+                viewFinder.postDelayed(videoCapture::stopRecording, 2000);
             });
 
             // Bind use cases to lifecycle
-            CameraX.bindToLifecycle((LifecycleOwner) MainActivity.this, preview, videoCapture);
+            CameraX.bindToLifecycle(MainActivity.this, preview, videoCapture);
         }
     };
-
-    private void updateTransform() {
-        Matrix matrix = new Matrix();
-
-        float centerX = viewFinder.getWidth() / 2f;
-        float centerY = viewFinder.getHeight() / 2f;
-
-        // Correct preview output to account for display rotation
-        float rotationDegrees;
-        switch (viewFinder.getDisplay().getRotation()) {
-            case Surface.ROTATION_0:
-                rotationDegrees = 0f;
-                break;
-            case Surface.ROTATION_90:
-                rotationDegrees = 90f;
-                break;
-            case Surface.ROTATION_180:
-                rotationDegrees = 180f;
-                break;
-            case Surface.ROTATION_270:
-                rotationDegrees = 270f;
-                break;
-            default:
-                return;
-        }
-
-        matrix.postRotate(-rotationDegrees, centerX, centerY);
-
-        // Finally, apply transformations to our TextureView
-        viewFinder.setTransform(matrix);
-    }
 
     /**
      * Process result from permission request dialog box, has the request
